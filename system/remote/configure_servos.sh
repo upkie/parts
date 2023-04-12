3a0eb5e0a287c4518bdea3590bd855d04bd9a289
Stéphane Caron <stephane.caron@normalesup.org>
2023-04-12 11:55:24 +0200

    Disable acceleration limits on all joints
    See https://github.com/mjbots/moteus/blob/38d688a933ce1584ee09f2628b5849d5e758ac21/docs/reference.md#servodefault_velocity_limit--servodefault_accel_limit

--- system/remote/configure_servos.sh
+++ system/remote/configure_servos.sh
@@ -34,3 +34,21 @@ echo "conf set servopos.position_min NaN" | sudo moteus_tool --pi3hat-cfg "1=1,2
 # Wheel velocity limits
 echo "conf set servo.max_velocity 10.0" | sudo moteus_tool --pi3hat-cfg "1=1,2,3;2=4,5,6" -t 3 -c
 echo "conf set servo.max_velocity 10.0" | sudo moteus_tool --pi3hat-cfg "1=1,2,3;2=4,5,6" -t 6 -c
+
+# Disable interpolation velocity limits on all joints
+# See https://github.com/mjbots/moteus/blob/38d688a933ce1584ee09f2628b5849d5e758ac21/docs/reference.md#servodefault_velocity_limit--servodefault_accel_limit
+echo "conf set servo.default_velocity_limit NaN" | sudo moteus_tool --pi3hat-cfg "1=1,2,3;2=4,5,6" -t 1 -c
+echo "conf set servo.default_velocity_limit NaN" | sudo moteus_tool --pi3hat-cfg "1=1,2,3;2=4,5,6" -t 2 -c
+echo "conf set servo.default_velocity_limit NaN" | sudo moteus_tool --pi3hat-cfg "1=1,2,3;2=4,5,6" -t 3 -c
+echo "conf set servo.default_velocity_limit NaN" | sudo moteus_tool --pi3hat-cfg "1=1,2,3;2=4,5,6" -t 4 -c
+echo "conf set servo.default_velocity_limit NaN" | sudo moteus_tool --pi3hat-cfg "1=1,2,3;2=4,5,6" -t 5 -c
+echo "conf set servo.default_velocity_limit NaN" | sudo moteus_tool --pi3hat-cfg "1=1,2,3;2=4,5,6" -t 6 -c
+
+# Disable acceleration limits on all joints
+# See https://github.com/mjbots/moteus/blob/38d688a933ce1584ee09f2628b5849d5e758ac21/docs/reference.md#servodefault_velocity_limit--servodefault_accel_limit
+echo "conf set servo.default_accel_limit NaN" | sudo moteus_tool --pi3hat-cfg "1=1,2,3;2=4,5,6" -t 1 -c
+echo "conf set servo.default_accel_limit NaN" | sudo moteus_tool --pi3hat-cfg "1=1,2,3;2=4,5,6" -t 2 -c
+echo "conf set servo.default_accel_limit NaN" | sudo moteus_tool --pi3hat-cfg "1=1,2,3;2=4,5,6" -t 3 -c
+echo "conf set servo.default_accel_limit NaN" | sudo moteus_tool --pi3hat-cfg "1=1,2,3;2=4,5,6" -t 4 -c
+echo "conf set servo.default_accel_limit NaN" | sudo moteus_tool --pi3hat-cfg "1=1,2,3;2=4,5,6" -t 5 -c
+echo "conf set servo.default_accel_limit NaN" | sudo moteus_tool --pi3hat-cfg "1=1,2,3;2=4,5,6" -t 6 -c
